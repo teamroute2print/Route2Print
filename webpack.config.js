717df0576ec0324663943cb0c3f28f9e47debf4c
Anudeep <anudeepgunda@gmail.com>
2018-07-03 23:19:31 -0400

    My first commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var config = {
+    entry: './APP.jsx',
+    output: {
+       path:'/',
+       filename: 'transpiled.js',
+    },
+    module: {
+       rules: [
+          {
+             test: /\.jsx?$/,
+             exclude: /node_modules/,
+             loader: 'babel-loader',
+             query: {
+                presets: ['es2015', 'react']
+             }
+          }
+       ]
+    }
+ }
+ module.exports = config;
