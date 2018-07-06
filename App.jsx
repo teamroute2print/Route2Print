22833bad6119caa0bc0bf1e3cf8a0c8da2d483f0
Anudeep <anudeepgunda@gmail.com>
2018-07-06 18:21:09 -0400

    initial push to dev branch

--- App.jsx
+++ App.jsx
@@ -4,10 +4,10 @@ import ReactDOM from 'react-dom';
 class MyComponent extends React.Component {
    render() {
       return (
-         <h2>
-            Hello World!!!
-         </h2>
-      );
+          <div>
+        <img  src="./images/logo.jpg" alt="logo" height="100" width="100"></img>
+        </div>
+    );
    }
 }
-ReactDOM.render(<MyComponent/>, document.getElementById('content'))
+ReactDOM.render(<MyComponent/>, document.getElementById('content'));
